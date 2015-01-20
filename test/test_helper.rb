dd199893854b30a15634d183b4b6ecb3fe7c88d4
Michael Weigle <michael.weigle@gmail.com>
2015-01-20 05:59:33 +0000

    Destroy All Fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,5 @@ CodeClimate::TestReporter.start
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
   # Add more helper methods to be used by all tests here...
 end

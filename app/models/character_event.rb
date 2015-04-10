bd1e154df0f6eabd8cb1ff36f2745cf53782fa56
Michael Weigle <michael.weigle@gmail.com>
2015-04-10 16:23:10 -0400

    Attending an event refreshes talent.investment_limit

--- app/models/character_event.rb
+++ app/models/character_event.rb
@@ -2,6 +2,11 @@ class CharacterEvent < ActiveRecord::Base
   has_paper_trail
   belongs_to :character
   belongs_to :event
-  
+
   accepts_nested_attributes_for :character, :event
+
+  after_create do |character_event|
+    current_character = Character.find_by(character_id: character_event.character_id)
+    current_character.talents.each { |talent| talent.investment_limit += 2; talent.save }
+  end
 end

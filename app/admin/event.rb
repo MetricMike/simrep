9adb49dd4e2b6bd7cad2cd94d18282ddffd1dacf
Michael Weigle <michael.weigle@gmail.com>
2015-06-19 21:17:10 -0700

    admin#event has attendance records

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register Event do
 
+  controller do
+    def scoped_collection
+      super.includes :characters, :character_events
+    end
+  end
+
   csv_importable :columns => [:campaign, :weekend, :play_exp, :clean_exp]
+
+  sidebar "Attending Characters", only: :show do
+    table_for event.character_events.order(params[:order].to_s.gsub(/(.*)(_)(.*)/, '\1 \3')), sortable: true do
+      column(:character, sortable: false) { |t| Character.find(t.character_id).name }
+      column :paid
+      column :cleaned
+    end
+  end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters

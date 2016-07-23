d6aa6ede7791bd4215cbbac2b5de2bbface294c4
Michael Weigle <michael.weigle@gmail.com>
2016-07-23 19:45:49 -0400

    oops forgot to stash pop

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,3 +1,5 @@
-ActiveRecordQueryTrace.enabled = !Rails.env.production?
-ActiveRecordQueryTrace.level = :app # default
-ActiveRecordQueryTrace.ignore_cached_queries = true # Default is false.
+if !Rails.env.production?
+  ActiveRecordQueryTrace.enabled = !Rails.env.production?
+  ActiveRecordQueryTrace.level = :app # default
+  ActiveRecordQueryTrace.ignore_cached_queries = true # Default is false.
+end

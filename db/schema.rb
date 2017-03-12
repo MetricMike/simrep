9396fff2123e2775ff406d92b5e1f3c1a8bdbb8a
Michael Weigle <michael.weigle@gmail.com>
2017-03-12 18:17:49 -0500

    bin and infra updates
    force redis to be in-memory
    less noisy logic for worker/tests

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20170307194314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"

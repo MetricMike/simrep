bf61daf49b3bfba0d9046e81ae16472af956fa27
Michael Weigle <michael.weigle@gmail.com>
2017-03-18 13:55:03 -0500

    don't persist rspec state on ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,9 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  if ENV['MTOWER'].present?
+    config.example_status_persistence_file_path = "spec/examples.txt"
+  end
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:

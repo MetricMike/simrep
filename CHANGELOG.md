59814a9e1a69bb4d841bbbac795173099ced5730
Michael Weigle <michael.weigle@gmail.com>
2017-09-14 19:45:19 -0400

    datetimepicker for special effects' expiration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 ## [Unreleased](#) - ????-??-??
 ### Added
 ### Fixed
+- fixed admin view of special_effects datefield
+
 ### Broke
 ### Changed
 ### Removed
--- app/admin/special_effect.rb
+++ app/admin/special_effect.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register SpecialEffect do
   menu false
+
+  form do |f|
+    f.inputs do
+      input :character
+      input :description
+      input :expiration, as: :date_time_picker, datepicker_options: { step: 15 }
+    end
+
+    f.actions
+  end
 end
